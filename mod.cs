97144dedf597aec6b1646657ecab338c7ca6654d
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-17 07:35:15 -0500

    Create mod.cs

---
+++ mod.cs
@@ -0,0 +1,5 @@
+GameObject.Find("motdtext").GetComponent<Text>().text = "<color=white>"Player Name: "player.NickName, " Player ID: "player.UserId,</color>"; // MOTD MAIN TEXT
+            GameObject.Find("COC Text").GetComponent<Text>().text = "<color=red>”test”</color>"; // CODE OF CONDUCT Text
+            GameObject.Find("CodeOfConduct").GetComponent<Text>().text = "<color=yellow> "InputHere"
+</color>"; // CODE OF CONDUCT TITLE TEXT
+            GameObject.Find("motd").GetComponent<Text>().text = "<color=yellow>I"nputHere"</color>"; // MOTD TITLE TEXT
