67507a230623884b1bff204ce267e541ab7780a9
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-17 07:36:08 -0500

    Update mod.cs

--- mod.cs
+++ mod.cs
@@ -1,5 +1,4 @@
-GameObject.Find("motdtext").GetComponent<Text>().text = "<color=white>"Player Name: "player.NickName, " Player ID: "player.UserId,</color>"; // MOTD MAIN TEXT
-            GameObject.Find("COC Text").GetComponent<Text>().text = "<color=red>”test”</color>"; // CODE OF CONDUCT Text
-            GameObject.Find("CodeOfConduct").GetComponent<Text>().text = "<color=yellow> "InputHere"
-</color>"; // CODE OF CONDUCT TITLE TEXT
-            GameObject.Find("motd").GetComponent<Text>().text = "<color=yellow>I"nputHere"</color>"; // MOTD TITLE TEXT
+GameObject.Find("motdtext").GetComponent<Text>().text = "<color=white>Player Name: " + player.NickName + " Player ID: " + player.UserId + "</color>"; // MOTD MAIN TEXT
+GameObject.Find("COC Text").GetComponent<Text>().text = "<color=red>test</color>"; // CODE OF CONDUCT Text
+GameObject.Find("CodeOfConduct").GetComponent<Text>().text = "<color=yellow>InputHere</color>"; // CODE OF CONDUCT TITLE TEXT
+GameObject.Find("motd").GetComponent<Text>().text = "<color=yellow>InputHere</color>"; // MOTD TITLE TEXT
