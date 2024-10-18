1604834fbf7a291e149a99513e78060885b3c3f5
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-18 08:00:46 -0500

    Update mod.cs

--- mod.cs
+++ mod.cs
@@ -11,4 +11,23 @@ class Mod
     GameObject.Find("motdtext").GetComponent<Text>().text = things
     GameObject.Find("motd").GetComponent<Text>().text = "<color=white>Players</color>";
   }
+  public string[] trackplayers = ""
+  public static void Tracker()
+  {
+    string[] trackPlayers = {
+      "uhh yea"
+    };
+    while (true) 
+    {
+      foreach (var player in PhotonNetwork.PlayerList)
+      {
+        if (trackPlayers.contains(player.Nickname.ToLower()))
+        {
+          string room = PhotonNetwork.CurrentRoom();
+          string message = $"{player.Nickname} found in {room}";
+          SendMessage(message)
+        }
+      }
+    }
+  }
 }
